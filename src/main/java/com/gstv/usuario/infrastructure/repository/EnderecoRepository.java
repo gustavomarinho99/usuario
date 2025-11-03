36f9a0da3448eb51f51653baa137809202e14c3d
Gustavo <gustavomarinho855@gmail.com>
2025-11-03 16:05:53 -0300

    adicionadas classes de security

---
+++ src/main/java/com/gstv/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.gstv.usuario.infrastructure.repository;
+
+import com.projetogustavo.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
